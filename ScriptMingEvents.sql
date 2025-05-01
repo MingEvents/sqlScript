9471487f9dd7bba437d9fcdb35154bfecf6e2a3f
Aimadpro <160835291+Aimadpro@users.noreply.github.com>
2025-05-01 19:56:30 +0200

    Update ScriptMingEvents.sql

--- ScriptMingEvents.sql
+++ ScriptMingEvents.sql
@@ -1,192 +1,94 @@
 DROP DATABASE IF EXISTS database_CULTURA_BCN;
 create database database_CULTURA_BCN;
-GO
-/****** Object:  Table [dbo].[Armchair]    Script Date: 30/04/2025 19:02:22 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Armchair](
-	[armchair_id] [int] IDENTITY(1,1) NOT NULL,
-	[columns] [int] NOT NULL,
-	[rows] [int] NOT NULL,
-	[user_id] [int] NULL,
-	[establish_id] [int] NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[armchair_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Chat]    Script Date: 30/04/2025 19:02:22 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Chat](
-	[chat_id] [int] IDENTITY(1,1) NOT NULL,
-	[send_date] [varchar](255) NULL,
-	[user1_id] [int] NULL,
-	[user2_id] [int] NULL,
- CONSTRAINT [pk_chats] PRIMARY KEY CLUSTERED 
-(
-	[chat_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[City]    Script Date: 30/04/2025 19:02:22 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[City](
-	[city_id] [int] IDENTITY(1,1) NOT NULL,
-	[name] [varchar](255) NOT NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[city_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Establishment]    Script Date: 30/04/2025 19:02:22 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Establishment](
-	[establish_id] [int] IDENTITY(1,1) NOT NULL,
-	[name] [varchar](50) NOT NULL,
-	[direction] [varchar](255) NOT NULL,
-	[capacity] [int] NOT NULL,
-	[city_id] [int] NOT NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[establish_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Event]    Script Date: 30/04/2025 19:02:22 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Event](
-	[event_id] [int] IDENTITY(1,1) NOT NULL,
-	[name] [varchar](255) NOT NULL,
-	[price] [int] NOT NULL,
-	[reserved_places] [int] NOT NULL,
-	[photo] [varchar](255) NULL,
-	[start_date] [varchar](255) NOT NULL,
-	[end_date] [varchar](255) NOT NULL,
-	[seating] [bit] NOT NULL,
-	[descripcion] [varchar](1000) NOT NULL,
-	[establish_id] [int] NOT NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[event_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Message]    Script Date: 30/04/2025 19:02:22 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Message](
-	[message_id] [int] IDENTITY(1,1) NOT NULL,
-	[sender_id] [int] NULL,
-	[content] [nvarchar](255) NULL,
-	[send_at] [varchar](255) NULL,
-	[is_read] [bit] NULL,
-	[chat_id] [int] NULL,
- CONSTRAINT [pk_messages] PRIMARY KEY CLUSTERED 
-(
-	[message_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Reserve_Ticket]    Script Date: 30/04/2025 19:02:22 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Reserve_Ticket](
-	[armchair_id] [int] NOT NULL,
-	[user_id] [int] NOT NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[armchair_id] ASC,
-	[user_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Role]    Script Date: 30/04/2025 19:02:22 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Role](
-	[role_id] [int] IDENTITY(1,1) NOT NULL,
-	[name] [varchar](25) NOT NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[role_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [dbo].[Users]    Script Date: 30/04/2025 19:02:22 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [dbo].[Users](
-	[user_id] [int] IDENTITY(1,1) NOT NULL,
-	[name] [varchar](50) NOT NULL,
-	[second_name] [varchar](150) NOT NULL,
-	[phone] [int] NOT NULL,
-	[password] [varchar](max) NOT NULL,
-	[photo] [varchar](255) NULL,
-	[email] [varchar](255) NOT NULL,
-	[role_id] [int] NOT NULL,
-PRIMARY KEY CLUSTERED 
-(
-	[user_id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
-UNIQUE NONCLUSTERED 
-(
-	[email] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-ALTER TABLE [dbo].[Armchair]  WITH CHECK ADD FOREIGN KEY([establish_id])
-REFERENCES [dbo].[Establishment] ([establish_id])
-GO
-ALTER TABLE [dbo].[Armchair]  WITH CHECK ADD FOREIGN KEY([user_id])
-REFERENCES [dbo].[Users] ([user_id])
-GO
-ALTER TABLE [dbo].[Chat]  WITH CHECK ADD FOREIGN KEY([user1_id])
-REFERENCES [dbo].[Users] ([user_id])
-GO
-ALTER TABLE [dbo].[Chat]  WITH CHECK ADD FOREIGN KEY([user2_id])
-REFERENCES [dbo].[Users] ([user_id])
-GO
-ALTER TABLE [dbo].[Establishment]  WITH CHECK ADD FOREIGN KEY([city_id])
-REFERENCES [dbo].[City] ([city_id])
-GO
-ALTER TABLE [dbo].[Event]  WITH CHECK ADD FOREIGN KEY([establish_id])
-REFERENCES [dbo].[Establishment] ([establish_id])
-GO
-ALTER TABLE [dbo].[Message]  WITH CHECK ADD  CONSTRAINT [fk_messages_chat] FOREIGN KEY([chat_id])
-REFERENCES [dbo].[Chat] ([chat_id])
-GO
-ALTER TABLE [dbo].[Message] CHECK CONSTRAINT [fk_messages_chat]
-GO
-ALTER TABLE [dbo].[Reserve_Ticket]  WITH CHECK ADD FOREIGN KEY([armchair_id])
-REFERENCES [dbo].[Armchair] ([armchair_id])
-GO
-ALTER TABLE [dbo].[Reserve_Ticket]  WITH CHECK ADD FOREIGN KEY([user_id])
-REFERENCES [dbo].[Users] ([user_id])
-GO
-ALTER TABLE [dbo].[Users]  WITH CHECK ADD FOREIGN KEY([role_id])
-REFERENCES [dbo].[Role] ([role_id])
-GO
+use database_CULTURA_BCN
+create table Establecimiento(
+id_establecimiento int IDENTITY(1,1) PRIMARY KEY,
+nombre varchar(50) not null,
+direccion varchar(255) not null,
+aforo int not null
+);
+
+
+create table rol(
+id_rol int IDENTITY(1,1) PRIMARY KEY,
+nombre varchar(25) not null
+);
+
+
+create table usuario(
+id_usuario int IDENTITY(1,1) PRIMARY KEY,
+nombre varchar(50) not null,
+apellidos varchar(150) not null,
+telefono int not null,
+contrasena varchar(MAX) not null,
+foto varchar(255),
+correo varchar(255) unique not null,
+id_rol int not null,
+FOREIGN KEY (id_rol) REFERENCES rol(id_rol)
+);
+
+
+
+
+create table evento(
+id_evento int IDENTITY(1,1) PRIMARY KEY,
+nombre varchar(255) not null,
+precio int not null,
+aforo int not null,
+edad_minima int not null,
+foto varchar(255),
+fecha_inicio date not null,
+fecha_fin date,
+enumeradas bit not null,
+filas int,
+columnas int,
+descripcion varchar(1000) not null
+);
+
+
+
+create table butaca(
+id_butaca int IDENTITY(1,1) PRIMARY KEY,
+columna int not null,
+fila int not null,
+id_evento int not null,
+id_usuario int
+FOREIGN KEY (id_evento) REFERENCES evento(id_evento),
+FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario)
+);
+
+
+
+create table chat (
+id_chat int identity(1,1) primary key,
+id_emisor int not null,
+id_receptor int not null,
+fecha_creacion date not null,
+FOREIGN KEY (id_emisor) REFERENCES usuario(id_usuario),
+FOREIGN KEY (id_receptor) REFERENCES usuario(id_usuario)
+);
+
+
+
+
+create table mensaje(
+id_mensaje int identity(1,1) primary key,
+id_usuario int,
+id_chat int,
+texto varchar(5000),
+fecha_publicacion date,
+FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
+FOREIGN KEY (id_chat) REFERENCES chat(id_chat)
+);
+
+
+
+
+create table reserva_entrada(
+id_butaca int,
+id_usuario int,
+PRIMARY KEY (id_butaca, id_usuario), 
+FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
+FOREIGN KEY (id_butaca) REFERENCES butaca(id_butaca)
+);
+
