bd405b05a4bc0bc882b11ad2d5dedf3c64304795
Aimadpro <160835291+Aimadpro@users.noreply.github.com>
2025-05-01 19:54:40 +0200

    Add files via upload

---
+++ ScriptMingEvents.sql
@@ -0,0 +1,191 @@
+USE [MingsEvents]
+GO
+/****** Object:  Table [dbo].[Armchair]    Script Date: 30/04/2025 19:02:22 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Armchair](
+	[armchair_id] [int] IDENTITY(1,1) NOT NULL,
+	[columns] [int] NOT NULL,
+	[rows] [int] NOT NULL,
+	[user_id] [int] NULL,
+	[establish_id] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[armchair_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Chat]    Script Date: 30/04/2025 19:02:22 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Chat](
+	[chat_id] [int] IDENTITY(1,1) NOT NULL,
+	[send_date] [varchar](255) NULL,
+	[user1_id] [int] NULL,
+	[user2_id] [int] NULL,
+ CONSTRAINT [pk_chats] PRIMARY KEY CLUSTERED 
+(
+	[chat_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[City]    Script Date: 30/04/2025 19:02:22 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[City](
+	[city_id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [varchar](255) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[city_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Establishment]    Script Date: 30/04/2025 19:02:22 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Establishment](
+	[establish_id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [varchar](50) NOT NULL,
+	[direction] [varchar](255) NOT NULL,
+	[capacity] [int] NOT NULL,
+	[city_id] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[establish_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Event]    Script Date: 30/04/2025 19:02:22 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Event](
+	[event_id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [varchar](255) NOT NULL,
+	[price] [int] NOT NULL,
+	[reserved_places] [int] NOT NULL,
+	[photo] [varchar](255) NULL,
+	[start_date] [varchar](255) NOT NULL,
+	[end_date] [varchar](255) NOT NULL,
+	[seating] [bit] NOT NULL,
+	[descripcion] [varchar](1000) NOT NULL,
+	[establish_id] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[event_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Message]    Script Date: 30/04/2025 19:02:22 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Message](
+	[message_id] [int] IDENTITY(1,1) NOT NULL,
+	[sender_id] [int] NULL,
+	[content] [nvarchar](255) NULL,
+	[send_at] [varchar](255) NULL,
+	[is_read] [bit] NULL,
+	[chat_id] [int] NULL,
+ CONSTRAINT [pk_messages] PRIMARY KEY CLUSTERED 
+(
+	[message_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Reserve_Ticket]    Script Date: 30/04/2025 19:02:22 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Reserve_Ticket](
+	[armchair_id] [int] NOT NULL,
+	[user_id] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[armchair_id] ASC,
+	[user_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Role]    Script Date: 30/04/2025 19:02:22 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Role](
+	[role_id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [varchar](25) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[role_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Users]    Script Date: 30/04/2025 19:02:22 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Users](
+	[user_id] [int] IDENTITY(1,1) NOT NULL,
+	[name] [varchar](50) NOT NULL,
+	[second_name] [varchar](150) NOT NULL,
+	[phone] [int] NOT NULL,
+	[password] [varchar](max) NOT NULL,
+	[photo] [varchar](255) NULL,
+	[email] [varchar](255) NOT NULL,
+	[role_id] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[user_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
+UNIQUE NONCLUSTERED 
+(
+	[email] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Armchair]  WITH CHECK ADD FOREIGN KEY([establish_id])
+REFERENCES [dbo].[Establishment] ([establish_id])
+GO
+ALTER TABLE [dbo].[Armchair]  WITH CHECK ADD FOREIGN KEY([user_id])
+REFERENCES [dbo].[Users] ([user_id])
+GO
+ALTER TABLE [dbo].[Chat]  WITH CHECK ADD FOREIGN KEY([user1_id])
+REFERENCES [dbo].[Users] ([user_id])
+GO
+ALTER TABLE [dbo].[Chat]  WITH CHECK ADD FOREIGN KEY([user2_id])
+REFERENCES [dbo].[Users] ([user_id])
+GO
+ALTER TABLE [dbo].[Establishment]  WITH CHECK ADD FOREIGN KEY([city_id])
+REFERENCES [dbo].[City] ([city_id])
+GO
+ALTER TABLE [dbo].[Event]  WITH CHECK ADD FOREIGN KEY([establish_id])
+REFERENCES [dbo].[Establishment] ([establish_id])
+GO
+ALTER TABLE [dbo].[Message]  WITH CHECK ADD  CONSTRAINT [fk_messages_chat] FOREIGN KEY([chat_id])
+REFERENCES [dbo].[Chat] ([chat_id])
+GO
+ALTER TABLE [dbo].[Message] CHECK CONSTRAINT [fk_messages_chat]
+GO
+ALTER TABLE [dbo].[Reserve_Ticket]  WITH CHECK ADD FOREIGN KEY([armchair_id])
+REFERENCES [dbo].[Armchair] ([armchair_id])
+GO
+ALTER TABLE [dbo].[Reserve_Ticket]  WITH CHECK ADD FOREIGN KEY([user_id])
+REFERENCES [dbo].[Users] ([user_id])
+GO
+ALTER TABLE [dbo].[Users]  WITH CHECK ADD FOREIGN KEY([role_id])
+REFERENCES [dbo].[Role] ([role_id])
+GO
